7699a89565fc967857eafedd109964935569901a
IGORnvk <novi0003@hz.nl>
2024-11-16 22:03:51 +0100

    Fix indentation

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,39 +1,14 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
-  import MediumIsland from './models/MediumIsland.svelte';
-  import LargeIsland from './models/LargeIsland.svelte';
-  import Tree from './models/Tree.svelte';
-  import LargeRock from './models/LargeRock.svelte';
-  import SmallRock from './models/SmallRock.svelte';
-  import WoodenSign from './models/WoodenSign.svelte';
-  import Grass from './models/Grass.svelte';
-  import Fire from './models/Fire.svelte';
-  import Campfire from './models/Campfire.svelte';
-  import Grass1 from './models/Grass1.svelte';
-    import StartingIsland from './StartingIsland.svelte';
-
-  const heightForItems = 1.4;
+  import { T } from "@threlte/core";
+  import { ContactShadows, Float, Grid, OrbitControls } from "@threlte/extras";
+  import StartingIsland from "./StartingIsland.svelte";
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 7, 7]}
-  fov={14}
->
-  <OrbitControls
-    enableZoom
-    enableDamping
-    zoomSpeed={3}
-    target.y={1.7}
-  />
+<T.PerspectiveCamera makeDefault position={[-10, 7, 7]} fov={14}>
+  <OrbitControls enableZoom enableDamping zoomSpeed={3} target.y={1.7} />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
+<T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
 
 <T.AmbientLight intensity={0.5} />
 
@@ -46,16 +21,8 @@
   cellSize={2}
 />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
+<ContactShadows scale={10} blur={2} far={2.5} opacity={0.5} />
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 0.3]}
->
+<Float floatIntensity={1} floatingRange={[0, 0.3]}>
   <StartingIsland />
 </Float>
--- src/lib/components/StartingIsland.svelte
+++ src/lib/components/StartingIsland.svelte
@@ -1,37 +1,56 @@
 <script lang="ts">
-    import heightForItems from "$lib/heightForItems";
-    import LargeIsland from "./models/LargeIsland.svelte";
-    import Tree from "./models/Tree.svelte";
-    import SmallRock from "./models/SmallRock.svelte";
-    import WoodenSign from "./models/WoodenSign.svelte";
-    import Grass1 from "./models/Grass1.svelte";
-    import FishingStand from "./models/FishingStand.svelte";
+  import heightForItems from "$lib/heightForItems";
+  import LargeIsland from "./models/LargeIsland.svelte";
+  import Tree from "./models/Tree.svelte";
+  import SmallRock from "./models/SmallRock.svelte";
+  import WoodenSign from "./models/WoodenSign.svelte";
+  import Grass1 from "./models/Grass1.svelte";
+  import FishingStand from "./models/FishingStand.svelte";
 
-    const treeSettings = [
-        { scale: 1, x: 0.2, y: $heightForItems, z: -0.5 },
-        { scale: 0.7, x: 0, y: $heightForItems, z: -0.3 },
-        { scale: 1.3, x: 0.3, y: $heightForItems, z: -0.1 },
-    ];
+  const treeSettings = [
+    { scale: 1, x: 0.2, y: $heightForItems, z: -0.5 },
+    { scale: 0.7, x: 0, y: $heightForItems, z: -0.3 },
+    { scale: 1.3, x: 0.3, y: $heightForItems, z: -0.1 },
+  ];
 
-    const grassCoords = [
-        { x: -0.1, y: $heightForItems, z: -0.35 },
-        { x: 0.2, y: $heightForItems, z: -0.1 },
-    ];
+  const grassCoords = [
+    { x: -0.1, y: $heightForItems, z: -0.35 },
+    { x: 0.2, y: $heightForItems, z: -0.1 },
+  ];
 </script>
 
 <LargeIsland />
 
 {#each treeSettings as tree}
-    <Tree scale={tree.scale} position.x={tree.x} position.y={tree.y} position.z={tree.z} />
+  <Tree
+    scale={tree.scale}
+    position.x={tree.x}
+    position.y={tree.y}
+    position.z={tree.z}
+  />
 {/each}
 
 {#each grassCoords as grass}
-    <Grass1 position.x={grass.x} position.y={grass.y} position.z={grass.z} />
+  <Grass1 position.x={grass.x} position.y={grass.y} position.z={grass.z} />
 {/each}
 
-<SmallRock scale={0.4} position.x={0.4} position.y={$heightForItems} position.z={0.2} />
+<SmallRock
+  scale={0.4}
+  position.x={0.4}
+  position.y={$heightForItems}
+  position.z={0.2}
+/>
 
+<FishingStand
+  position.x={0.1}
+  position.y={$heightForItems}
+  position.z={0.4}
+  rotation.y={1}
+/>
 
-<FishingStand position.x={0.1} position.y={$heightForItems} position.z={0.4} rotation.y={1} />
-
-<WoodenSign scale={0.2} position.x={-0.1} position.y={$heightForItems} rotation.y={-0.9} />
+<WoodenSign
+  scale={0.2}
+  position.x={-0.1}
+  position.y={$heightForItems}
+  rotation.y={-0.9}
+/>
