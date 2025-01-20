bb04c9229679cf81da2fd7a29178e1113ea3cc9e
IGORnvk <novi0003@hz.nl>
2025-01-20 22:57:08 +0100

    Support dynamic content

--- src/lib/components/DialogPopup.svelte
+++ src/lib/components/DialogPopup.svelte
@@ -6,6 +6,7 @@
   import { dialogWindow } from "$lib/stores";
   import "augmented-ui/augmented-ui.min.css";
 
+  export let content = '';
   export const ref = new Group();
   const { camera } = useThrelte();
 
@@ -20,6 +21,7 @@
 <HTML
   {ref}
   transform
+  sprite
 >
 <div class="w-8 h-1 bg-main absolute top-0.5 z-10"></div>
 <div class="w-8 h-1 bg-main absolute top-0.5 z-10 -right-0"></div>
@@ -27,7 +29,7 @@
 <div class="w-8 h-1 bg-main absolute bottom-0.5 z-10 right-0"></div>
   <div
     data-augmented-ui="both"
-    class="inline-block w-auto max-w-3xl p-7 font-bold sm:text-xl text-main uppercase"
+    class="inline-block w-auto max-w-xl p-7 font-bold sm:text-xl text-main uppercase break-words"
     style="
           --aug-inlay-bg: #082f49;
           --aug-border-bg: #4cd0fc;
@@ -38,13 +40,6 @@
           --aug-border-x: 0px;
           "
   >
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industry's standard dummy text ever since the
-    1500s, when an unknown printer took a galley of type and scrambled it to
-    make a type specimen book. It has survived not only five centuries, but also
-    the leap into electronic typesetting, remaining essentially unchanged. It
-    was popularised in the 1960s with the release of Letraset sheets containing
-    Lorem Ipsum passages, and more recently with desktop publishing software
-    like Aldus PageMaker including versions of Lorem Ipsum.
+    { content }
   </div>
 </HTML>
