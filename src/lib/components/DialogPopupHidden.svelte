5ca08c4170ad0da7108df715566a3af6b958e244
IGORnvk <novi0003@hz.nl>
2025-02-08 21:47:49 +0100

    Revert unnecessary logic

--- src/lib/components/DialogPopupHidden.svelte
+++ src/lib/components/DialogPopupHidden.svelte
@@ -12,13 +12,8 @@
     camera.current.add(ref);
     ref.position.set(0, -14.5, -55);
 
-    const container = document.getElementById('dialogWindowContainerHidden');
-    const viewportDiff = window.innerHeight - window.visualViewport?.height;
-
-    // Add padding only when there is a viewport difference.
-    if (viewportDiff != 0) container.style.marginBottom = `${viewportDiff}px`;
-
     // Cleanup logic for animations.
+    const container = document.getElementById('dialogWindowContainerHidden');
     container?.addEventListener('animationend', (event) => {
       if (event.animationName === 'appear') {
         container.classList.remove('animate-appear');
